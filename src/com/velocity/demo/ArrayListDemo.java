1fc17dde3de0f0efb602a400c18bd5cf240c57aa
pchavan1954 <ptchavan99@gmail.com>
2022-12-24 23:18:37 +0530

    first commit

---
+++ src/com/velocity/demo/ArrayListDemo.java
@@ -0,0 +1,14 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("ajay");
+		list.add("vijay");
+		list.add("sanjay";)
+	}
+}
